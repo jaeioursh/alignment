b476f5c2952209f567a59f60589a93979c5d38eb
Samantha Kammerer <Sam@10-248-88-87.wireless.oregonstate.edu>
2023-06-27 14:49:33 -0700

    updated Sample Size to 5000

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 from alignment import load_data,eval
 import numpy as np
 
-sample_size = 100
+sample_size = 5000
 Reward_Data= np.zeros((sample_size,2))
 State_Array= np.zeros((sample_size,3))
 Alignment_Data= np.zeros((sample_size,3))
