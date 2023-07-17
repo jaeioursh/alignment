7f79c9d349519dc8a059203e28ecdbb710ef85d3
Samantha Kammerer <Sam@10-248-88-87.wireless.oregonstate.edu>
2023-07-17 11:51:28 -0700

    Generational Analysis

--- analysis.py
+++ analysis.py
@@ -10,65 +10,72 @@ Calc= np.zeros((sample_size,2))
 count=0
 row_index=0
 index=0
+generation=50
 
 file=open('SSAnalysis', 'wb')
 
-if __name__=="__main__":
-    agent_idx=0 #which agent
-    team_idx=2 #which team
-    env,pos,teams,net=load_data(n_agents=5,agent_idx=agent_idx,n_actors=4,iteration=0,generation=3000)
-    
-    for row_index in range (sample_size):
-        x=np.random.uniform(-5,35) # -5 to 35 ish
-        y=np.random.uniform(-5,35) # -5 to 35 ish
-        t=np.random.uniform(-np.pi,np.pi) #-pi to pi
-        state,G = eval(x,y,t,team_idx,agent_idx,env,pos,teams)
-        G_estimate=net.feed(state)[0,0]
-        State_Array[row_index,0]=x
-        State_Array[row_index,1]=y
-        State_Array[row_index,2]=t
-        Reward_Data[row_index,0] = G
-        Reward_Data[row_index,1] = G_estimate
+for generation in range (4000): 
+
+
+    if __name__=="__main__":
+        agent_idx=0 #which agent
+        team_idx=2 #which team
+        env,pos,teams,net=load_data(n_agents=5,agent_idx=agent_idx,n_actors=4,iteration=0, generation=generation)
         
-    while index < sample_size:
-        test_index=np.random.randint(0,(sample_size-1))
-        row_index=np.random.randint(0,(sample_size-1))
-        G1 = Reward_Data[row_index,0]
-        G2 = Reward_Data[test_index,0]
-        
-        while G1==G2:
-            test_index=np.random.randint(0,sample_size-1)
-            G2 = Reward_Data[test_index,0]
-            if count==(sample_size-1):
-                break
-            count=count+1
+        for row_index in range (sample_size):
+            x=np.random.uniform(-5,35) # -5 to 35 ish
+            y=np.random.uniform(-5,35) # -5 to 35 ish
+            t=np.random.uniform(-np.pi,np.pi) #-pi to pi
+            state,G = eval(x,y,t,team_idx,agent_idx,env,pos,teams)
+            G_estimate=net.feed(state)[0,0]
+            State_Array[row_index,0]=x
+            State_Array[row_index,1]=y
+            State_Array[row_index,2]=t
+            Reward_Data[row_index,0] = G
+            Reward_Data[row_index,1] = G_estimate
             
-        if count != sample_size:
-            GE1 = Reward_Data[row_index,1]
-            GE2 = Reward_Data[test_index,1]
-
-            aligned= (GE1 - GE2)*(G1 - G2)
-            if aligned in Calc:
-                break
+        while index < sample_size:
+            test_index=np.random.randint(0,(sample_size-1))
+            row_index=np.random.randint(0,(sample_size-1))
+            G1 = Reward_Data[row_index,0]
+            G2 = Reward_Data[test_index,0]
+            
+            while G1==G2:
+                test_index=np.random.randint(0,sample_size-1)
+                G2 = Reward_Data[test_index,0]
+                if count==(sample_size-1):
+                    break
+                count=count+1
                 
-            if aligned > 0:
-                Alignment_Data[index,0]=1
-            elif aligned < 0:
-                Alignment_Data[index,0]=0
+            if count != sample_size:
+                GE1 = Reward_Data[row_index,1]
+                GE2 = Reward_Data[test_index,1]
 
-            Calc[index]=aligned
-            Alignment_Data[index,1]=row_index
-            Alignment_Data[index,2]=test_index
+                aligned= (GE1 - GE2)*(G1 - G2)
+                if aligned in Calc:
+                    break
+                    
+                if aligned > 0:
+                    Alignment_Data[index,0]=1
+                elif aligned < 0:
+                    Alignment_Data[index,0]=0
 
-            index=index+1
+                Calc[index]=aligned
+                Alignment_Data[index,1]=row_index
+                Alignment_Data[index,2]=test_index
 
-    #Write data into pickle file
-    pickle.dump(Alignment_Data,file)
+                index=index+1
 
-    #Sum the aligned data and calculate percent aligned
-    Sum_Aligned= Alignment_Data[:,0].sum() 
-    Percent_Aligned= (Sum_Aligned/(index+1)) *100
+        #Sum the aligned data and calculate percent aligned
+        Sum_Aligned= Alignment_Data[:,0].sum() 
+        Percent_Aligned= (Sum_Aligned/(index+1)) *100
 
-    print('%', Percent_Aligned)
+        #Write data into pickle file
+        pickle.dump(Percent_Aligned,file)
 
-    file.close()
+        print('%', Percent_Aligned)
+
+        generation= generation + 50
+
+file.close()
+        
