2c9d7b48378907680e4946a8714f05e6fd7a1f7f
Samantha Kammerer <Sam@10-248-88-87.wireless.oregonstate.edu>
2023-08-09 13:36:40 -0700

    Parrell

--- SSanalysis.py
+++ SSanalysis.py
@@ -3,6 +3,7 @@ import numpy as np
 import pickle
 from os import killpg
 import os
+import multiprocessing as mp
 
 def save (team_idx, agent_idx,Percent_Aligned_arr):
     folder="SS/"
@@ -15,6 +16,79 @@ def save (team_idx, agent_idx,Percent_Aligned_arr):
     pickle.dump(Percent_Aligned_arr,file)
     file.close
 
+def SingleState (team_idx,agent_idx):
+    sample_size = 10000 #Cannot be lower then 100, or else randint fuction freezes
+    Reward_Data= np.zeros((sample_size,2))
+    Percent_Aligned_arr= np.zeros((4001,1))
+    Alignment_Data= np.zeros((sample_size,3))
+    Calc= np.zeros((sample_size,2))
+    count=0
+    index=0
+    
+    for g in range (0,4001):
+        if g%50==0:
+            env,pos,teams,net=load_data(n_agents=5,agent_idx=agent_idx,n_actors=4,iteration=0, generation=g)
+        for row_index in range (0,sample_size):
+            x=np.random.uniform(-5,35) # -5 to 35 ish
+            y=np.random.uniform(-5,35) # -5 to 35 ish
+            t=np.random.uniform(-np.pi,np.pi) #-pi to pi
+            state,G = eval(x,y,t,team_idx,agent_idx,env,pos,teams,generation=g)
+            G_estimate=net.feed(state)[0,0]
+            Reward_Data[row_index,0] = G
+            Reward_Data[row_index,1] = G_estimate
+                        
+        while index < sample_size:
+            test_index=np.random.randint(0,(sample_size-1))
+            row_index=np.random.randint(0,(sample_size-1))
+            G1 = Reward_Data[row_index,0]
+            G2 = Reward_Data[test_index,0]
+                        
+            while G1==G2:
+                test_index=np.random.randint(0,sample_size-1)
+                G2 = Reward_Data[test_index,0]
+                if count==(sample_size-1):
+                    break
+                count=count+1
+                                
+            if count != sample_size:
+                GE1 = Reward_Data[row_index,1]
+                GE2 = Reward_Data[test_index,1]
+
+                aligned= (GE1 - GE2)*(G1 - G2)
+                if aligned in Calc:
+                    break
+                                    
+                if aligned > 0:
+                    Alignment_Data[index,0]=1
+                elif aligned < 0:
+                    Alignment_Data[index,0]=0
+
+                Calc[index]=aligned
+                Alignment_Data[index,1]=row_index
+                Alignment_Data[index,2]=test_index
+
+                index=index+1
+
+        #Sum the aligned data and calculate percent aligned
+        Sum_Aligned= Alignment_Data[:,0].sum() 
+        Percent_Aligned= (Sum_Aligned/(index+1)) *100
+
+        print('%', Percent_Aligned,g)
+            
+        Percent_Aligned_arr [g]= Percent_Aligned
+
+        #reset arrays
+        Reward_Data= np.zeros((sample_size,2))
+        Alignment_Data= np.zeros((sample_size,3))
+        Calc= np.zeros((sample_size,2))
+        count=0
+        index=0
+
+        #Write data into pickle file
+    save(team_idx,agent_idx,Percent_Aligned_arr)
+    Percent_Aligned_arr= np.zeros((4001,1))
+
+
 #-----------------------------
 
 sample_size = 10000 #Cannot be lower then 100, or else randint fuction freezes
@@ -26,76 +100,16 @@ count=0
 index=0
 tna=np.array ([[0,1,2,3],[0,1,2,4],[0,1,3,4],[0,2,3,4],[1,2,3,4]])
 
-team_idx= 0
-
-for team_idx in range (0,5):
-#Cycle through team indexes
-    for cidx in range (0,4):
-        agent_idx= tna[team_idx,cidx]
-        print(agent_idx,team_idx)
-        for g in range (0,4001):
-            if g%50==0:
-                if __name__=="__main__":
-                    env,pos,teams,net=load_data(n_agents=5,agent_idx=agent_idx,n_actors=4,iteration=0, generation=g)
-
-            for row_index in range (0,sample_size):
-                x=np.random.uniform(-5,35) # -5 to 35 ish
-                y=np.random.uniform(-5,35) # -5 to 35 ish
-                t=np.random.uniform(-np.pi,np.pi) #-pi to pi
-                state,G = eval(x,y,t,team_idx,agent_idx,env,pos,teams,generation=g)
-                G_estimate=net.feed(state)[0,0]
-                Reward_Data[row_index,0] = G
-                Reward_Data[row_index,1] = G_estimate
-                        
-            while index < sample_size:
-                test_index=np.random.randint(0,(sample_size-1))
-                row_index=np.random.randint(0,(sample_size-1))
-                G1 = Reward_Data[row_index,0]
-                G2 = Reward_Data[test_index,0]
-                        
-                while G1==G2:
-                    test_index=np.random.randint(0,sample_size-1)
-                    G2 = Reward_Data[test_index,0]
-                    if count==(sample_size-1):
-                        break
-                    count=count+1
-                                
-                if count != sample_size:
-                    GE1 = Reward_Data[row_index,1]
-                    GE2 = Reward_Data[test_index,1]
-
-                    aligned= (GE1 - GE2)*(G1 - G2)
-                    if aligned in Calc:
-                        break
-                                    
-                    if aligned > 0:
-                        Alignment_Data[index,0]=1
-                    elif aligned < 0:
-                        Alignment_Data[index,0]=0
-
-                    Calc[index]=aligned
-                    Alignment_Data[index,1]=row_index
-                    Alignment_Data[index,2]=test_index
-
-                    index=index+1
-            #Sum the aligned data and calculate percent aligned
-            Sum_Aligned= Alignment_Data[:,0].sum() 
-            Percent_Aligned= (Sum_Aligned/(index+1)) *100
-
-            print('%', Percent_Aligned,g)
-            
-            Percent_Aligned_arr [g]= Percent_Aligned
-
-            #reset arrays
-            Reward_Data= np.zeros((sample_size,2))
-            Alignment_Data= np.zeros((sample_size,3))
-            Calc= np.zeros((sample_size,2))
-            count=0
-            index=0
-
-        #Write data into pickle file
-        save(team_idx,agent_idx,Percent_Aligned_arr)
-        Percent_Aligned_arr= np.zeros((4001,1))
+if __name__ == '__main__':
+    for team_idx in range (0,5):
+    #Cycle through team indexes
+        for cidx in range (0,4):
+            agent_idx= tna[team_idx,cidx]
+            print(agent_idx,team_idx)
+            p=mp.Process(target=SingleState,args=(team_idx,agent_idx))
+            p.start()
+    
+    p.join
 
 
                 
