5114dc005bbf37e443e790d7aac1f7718cf2aa75
Samantha Kammerer <Sam@MacBook-Pro-3.local>
2023-08-11 18:45:58 -0700

    done

--- t-analysis.py
+++ t-analysis.py
@@ -27,7 +27,7 @@ bigG=np.zeros((30,numtraj)) #Global Rewards for all trajectories
 Alignment_Data= np.zeros(numtraj) #To store the alignment calculations
 Calc= np.zeros(numtraj)
 GlobRwrds=np.zeros(numtraj) 
-Percent_Aligned_arr= np.zeros((81,2))
+Percent_Aligned_arr= np.zeros((81,5))
 ErrArr=np.zeros(100)
 index=0
 count=0
@@ -36,67 +36,78 @@ gen=np.zeros((81))
 for i in range(0,81):
  gen[i]=i*50
 
-for g in range (0,4001,50):
-    lilg,bigG=getarr(agent_idx,g)
-    LocRwrds=np.max(lilg, axis=0)    
-    GCoods=np.argmax(lilg, axis=0)
+for agent_idx in range (0,5):
+    for g in range (0,4001,50):
+            lilg,bigG=getarr(agent_idx,g)
+            LocRwrds=np.max(lilg, axis=0)    
+            GCoods=np.argmax(lilg, axis=0)
 
-    for i in range (0,(4*50)):
-        GlobRwrds[i]=bigG[GCoods[i],i]
+            for i in range (0,(4*50)):
+                GlobRwrds[i]=bigG[GCoods[i],i]
 
-    for x in range (0,100):
-        while index < numtraj:
-            idx1=np.random.randint(0,(numtraj-1))
-            idx2=np.random.randint(0,(numtraj-1))
-            GR1 = GlobRwrds[idx1]
-            GR2 = GlobRwrds[idx2]
-                            
-            while GR1==GR2:
-                idx2=np.random.randint(0,(numtraj-1))
-                GR2 = GlobRwrds[idx2]
-                if count==(numtraj-1):
-                    break
-                count=count+1
-                                
-            if count != numtraj:
-                LR1 = LocRwrds[idx1]
-                LR2 = LocRwrds[idx2]
+            for x in range (0,100):
+                while index < numtraj:
+                    idx1=np.random.randint(0,(numtraj-1))
+                    idx2=np.random.randint(0,(numtraj-1))
+                    GR1 = GlobRwrds[idx1]
+                    GR2 = GlobRwrds[idx2]
+                                    
+                    while GR1==GR2:
+                        idx2=np.random.randint(0,(numtraj-1))
+                        GR2 = GlobRwrds[idx2]
+                        if count==(numtraj-1):
+                            break
+                        count=count+1
+                                        
+                    if count != numtraj:
+                        LR1 = LocRwrds[idx1]
+                        LR2 = LocRwrds[idx2]
 
-                aligned= (LR1 - LR2)*(GR1 - GR2)
-                if aligned in Calc:
-                    break
-                if aligned > 0:
-                    Alignment_Data[index]=1
-                elif aligned < 0:
-                    Alignment_Data[index]=0
+                        aligned= (LR1 - LR2)*(GR1 - GR2)
+                        if aligned in Calc:
+                            break
+                        if aligned > 0:
+                            Alignment_Data[index]=1
+                        elif aligned < 0:
+                            Alignment_Data[index]=0
 
-                Calc[index]=aligned
+                        Calc[index]=aligned
 
-                index=index+1
+                        index=index+1
 
-        #Sum the aligned data and calculate percent aligned
-        Sum_Aligned= Alignment_Data[:].sum() 
-        ErrArr[x]= (Sum_Aligned/(index+1)) *100
+                #Sum the aligned data and calculate percent aligned
+                Sum_Aligned= Alignment_Data[:].sum() 
+                ErrArr[x]= (Sum_Aligned/(index+1)) *100
 
-        #reset arrays
-        Alignment_Data= np.zeros(numtraj) #To store the alignment calculations
-        Calc= np.zeros(numtraj)
-        count=0
-        index=0
+                #reset arrays
+                Alignment_Data= np.zeros(numtraj) #To store the alignment calculations
+                Calc= np.zeros(numtraj)
+                count=0
+                index=0
+            if g%50==0:
+                h=g//50
+                Percent_Aligned_arr[h,agent_idx]=np.mean(ErrArr)
 
-    h=g//50
-    Percent_Aligned_arr[h,0]=np.mean(ErrArr)
 
-y= Percent_Aligned_arr[:,0]
+y0=Percent_Aligned_arr[:,0]
+y1=Percent_Aligned_arr[:,1]
+y2=Percent_Aligned_arr[:,2]
+y3=Percent_Aligned_arr[:,3]
+y4=Percent_Aligned_arr[:,4]
 
-plt.plot(gen, y, color='green', label = 'Agent' + ' ' + str(agent_idx))
+
+plt.plot(gen, y0, color='green',linestyle='solid',label = 'Agent' + ' ' + '0')
+plt.plot(gen, y1, color='purple',linestyle='solid',label = 'Agent' + ' ' + '1')
+plt.plot(gen, y2, color='blue', linestyle='solid',label = 'Agent' + ' ' + '2')
+plt.plot(gen, y3, color='red',linestyle='solid',label = 'Agent' + ' ' + '3')
+plt.plot(gen, y4, color='orange',linestyle='solid',label = 'Agent' + ' ' + '4')
 
 plt.ylim([0,100])
 plt.xlim([0,4000])        
 plt.legend(loc='upper left')
 plt.xlabel("Generation")
 plt.ylabel("Percent Alignment")
-plt.title("Percent Alignment for Agent " + str(agent_idx))
+plt.title("Percent Alignment of Trajectories")
 
 
 plt.show()
